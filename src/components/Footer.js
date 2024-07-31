092918484714d364e7648c11ba2ef8907b8071f6
Argha Mallick <arghamallick5@gmail.com>
2024-07-31 23:31:54 +0530

    Disclaimer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,13 @@ import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-300 dark:bg-gray-700 text-white py-4">
+        <footer className="bg-gray-300 dark:bg-gray-700 text-white py-1">
+            <div className="container mx-auto text-center relative overflow-hidden">
+                <div className="scrolling-text text-gray-300 text-sm">
+                    <span className="underline">Disclaimer</span>: This <Link href={'/'}>Arcade Points Calculator</Link> system is an unofficial release and is not supported by Google. While every effort has been made to ensure the accuracy and functionality of this system, it is provided on an "as-is" basis without any guarantees. The developer of this system is not responsible for any inaccuracies in the Arcade Points once the Prize Counter opens. For official information and support, please refer to the <Link href={'https://www.cloudskillsboost.google/'} target='_blank'>GCSB chat support</Link>.
+                </div>
+            </div>
+            <hr className='my-1' />
             <div className="container mx-auto px-4 flex flex-col lg:flex-row-reverse items-center justify-center lg:justify-between">
                 <div className="my-2 flex justify-center items-center">
                     <Link href='https://github.com/iamarghamallick' target='_blank'><FaGithub size={30} className='mx-2 text-gray-900 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white' /></Link>
